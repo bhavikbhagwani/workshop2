e6cfc702d332949a00dac6da6ed7f68b6136fb04
Ludvig <ludvig.dahlberg0006@stud.hkr.se>
2024-02-05 14:09:29 +0100

    Added another print

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     name = input("Enter your name: ")
     print(f"Hello {name}!")
     print("I printed your name above")
+    print(" I like dogs")
 
 if __name__ == "__main__":
     main()
