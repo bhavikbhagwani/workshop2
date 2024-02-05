3b9dc5cb6b30e98a6125ee1ae69f849d3514fda0
Jacobs Kasarachi <jacobskasarachi@gmail.com>
2024-02-05 14:23:05 +0100

    age added

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
     name = input("Enter your name: ")
     print(f"Hello {name}!")
     print("I printed your name above")
+    age = input("How old are you? ")
+    print(f"youre {age} years old!")
     print(" I like dogs")
+
 
 if __name__ == "__main__":
     main()
